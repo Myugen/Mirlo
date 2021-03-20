1b663268fb15d83199b212b9b90813bf0c3e3133
alephshahor <adrianalvarezleon@gmail.com>
2021-03-20 23:46:06 +0000

    feat(all): Add Prettier, Eslint, i18js and Abs path

---
+++ client/src/routes/signup/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from 'src/components/header'
+
+const SignUp = () => {
+  return (
+    <div class="w-screen h-screen bg-gray-800">
+      <Header />
+    </div>
+  )
+}
+
+export default SignUp
---
+++ client/src/translations/es/index.js
@@ -0,0 +1,8 @@
+export const TRANSLATIONS_ES = {
+  common: {
+    title: 'Mirlo',
+  },
+  logIn: {
+    title: 'Iniciar sesión',
+  },
+}

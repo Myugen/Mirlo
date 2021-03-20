926d592908d065f51052a52a26569a03e444f241
alephshahor <adrianalvarezleon@gmail.com>
2021-03-20 23:48:26 +0000

    feat(components): Implement <Header>

---
+++ client/src/components/header/index.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import styled from 'styled-components'
+import { useTranslation } from 'react-i18next'
+import 'src/translations/i18n'
+
+const Title = styled.div`
+  flex: 3;
+  display: flex;
+  justify-content: flex-start;
+`
+
+const Login = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+`
+
+const TitleButton = styled.button`
+  font-size: 1.5rem;
+  text-align: center;
+  color: white;
+  font-family: 'Source Code Pro', monospace;
+  font-weight: 600;
+
+  &:focus {
+    outline: none;
+  }
+`
+const LoginButton = styled.button`
+  font-size: 1rem;
+  text-align: center;
+  color: white;
+  font-family: 'Source Code Pro', monospace;
+
+  &:focus {
+    outline: none;
+  }
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  background-color: rgba(31, 41, 55, 1);
+  padding: 10px 20px 0px 20px;
+  overflow: hidden;
+`
+
+const Header = () => {
+  const { t } = useTranslation()
+
+  return (
+    <Wrapper>
+      <Title>
+        <TitleButton>{t('common.title')}</TitleButton>
+      </Title>
+      <Login>
+        <LoginButton>{t('logIn.title')}</LoginButton>
+      </Login>
+    </Wrapper>
+  )
+}
+
+export default Header

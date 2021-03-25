fdfd0e649b76f6889cb96472da071f02cf5cc5c6
alephshahor <adrianalvarezleon@gmail.com>
2021-03-25 22:21:10 +0000

    feat(errors): Add errors directory and implement APIError

---
+++ server/errors/all.go
@@ -0,0 +1,27 @@
+package errors
+
+import (
+	"github.com/alephshahor/Mirlo/server/enums/languages"
+	"github.com/alephshahor/Mirlo/server/types"
+	"net/http"
+)
+
+type APIError struct {
+	HTTPStatusCode int
+	Message        map[types.LanguageKey]string
+}
+
+var (
+	ErrEmailAlreadyRegistered = APIError{
+		HTTPStatusCode: http.StatusConflict,
+		Message: map[types.LanguageKey]string{
+			languages.Spanish: "Ya existe una cuenta registrada con este email",
+			languages.English: "An account registered with this email already exists",
+		},
+	}
+)
+
+// Implementing this method so the Error interface is satisfied
+func (e APIError) Error() string {
+	return e.Message[languages.English]
+}

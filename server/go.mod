c5a103f68a51e9ceac77d8c52c8c154f153c95bf
alephshahor <adrianalvarezleon@gmail.com>
2021-03-25 19:37:11 +0000

    feat(all): Configure CORS middleware

--- server/go.mod
+++ server/go.mod
@@ -3,6 +3,7 @@ module github.com/alephshahor/Mirlo/server
 go 1.16
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/go-pg/pg/v10 v10.8.0
 	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo v3.3.10+incompatible
--- server/main.go
+++ server/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"github.com/alephshahor/Mirlo/server/handlers"
 	"github.com/alephshahor/Mirlo/server/services"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/middleware"
+	"os"
 
 	"github.com/labstack/echo"
 )
 
 func main() {
+	configFilePath := os.Getenv("GOPATH") + "/src/github.com/alephshahor/Mirlo/server/.env"
+	if err := godotenv.Load(configFilePath); err != nil {
+		panic(fmt.Errorf("Fatal error loading .env file: %s \n", err))
+	}
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000"},
+	}))
 	handlers.InitializeHandlers(e, services.Services())
 	e.Logger.Fatal(e.Start(":8080"))
 }

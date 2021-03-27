d73c8230ff3302aa051493112cfc9f39ba6017ce
alephshahor <adrianalvarezleon@gmail.com>
2021-03-27 16:45:37 +0000

    refactor(requests): Improve NewUserRequests test

--- server/requests/user.go
+++ server/requests/user.go
@@ -2,6 +2,6 @@ package requests
 
 type NewUser struct {
 	UserName string `json:"username" validate:"required,gt=0,lte=20"`
-	Password string `json:"password" validate:"required"`
-	Email    string `json:"email" validate:"required,email,gt=0,lte=320"`
+	Password string `json:"password" validate:"required,gte=16,lte=128"`
+	Email    string `json:"email" validate:"required,email"`
 }
--- server/requests/user_test.go
+++ server/requests/user_test.go
@@ -23,29 +23,72 @@ func (suite *UserRequestTestSuite) SetupTest() {
 }
 
 func (suite *UserRequestTestSuite) TestNewUserRequestValidator() {
+	var err error
 	var validate = validator.New()
-	var correctNewUserReq = &NewUser{
+	var validationErrs validator.ValidationErrors
+	var newUserReq NewUser
+	var correctNewUserReq = NewUser{
 		UserName: utils.RandString(16),
 		Password: utils.RandString(16),
 		Email:    fmt.Sprintf("%v@email.com", utils.RandString(16)),
 	}
 	assert.Nil(suite.T(), validate.Struct(correctNewUserReq))
 
-	var newUserReq = correctNewUserReq
+	// Empty UserName
+	newUserReq = correctNewUserReq
 	newUserReq.UserName = ""
-	assert.NotNil(suite.T(), validate.Struct(newUserReq))
+	err = validate.Struct(newUserReq)
+	validationErrs = err.(validator.ValidationErrors)
+	assert.Len(suite.T(), validationErrs, 1)
+	assert.Equal(suite.T(), "required", validationErrs[0].Tag())
 
+	// Long UserName
+	newUserReq = correctNewUserReq
+	newUserReq.UserName = utils.RandString(21)
+	err = validate.Struct(newUserReq)
+	validationErrs = err.(validator.ValidationErrors)
+	assert.Len(suite.T(), validationErrs, 1)
+	assert.Equal(suite.T(), "lte", validationErrs[0].Tag())
+
+	// Empty Email
 	newUserReq = correctNewUserReq
 	newUserReq.Email = ""
-	assert.NotNil(suite.T(), validate.Struct(newUserReq))
+	err = validate.Struct(newUserReq)
+	validationErrs = err.(validator.ValidationErrors)
+	assert.Len(suite.T(), validationErrs, 1)
+	assert.Equal(suite.T(), "required", validationErrs[0].Tag())
 
+	// Email
 	newUserReq = correctNewUserReq
-	newUserReq.Email = utils.RandString(16)
-	assert.NotNil(suite.T(), validate.Struct(newUserReq))
+	newUserReq.Email = "111"
+	err = validate.Struct(newUserReq)
+	validationErrs = err.(validator.ValidationErrors)
+	assert.Len(suite.T(), validationErrs, 1)
+	assert.Equal(suite.T(), "email", validationErrs[0].Tag())
 
+	// Empty Password
 	newUserReq = correctNewUserReq
 	newUserReq.Password = ""
-	assert.NotNil(suite.T(), validate.Struct(newUserReq))
+	err = validate.Struct(newUserReq)
+	validationErrs = err.(validator.ValidationErrors)
+	assert.Len(suite.T(), validationErrs, 1)
+	assert.Equal(suite.T(), "required", validationErrs[0].Tag())
+
+	// Short Password
+	newUserReq = correctNewUserReq
+	newUserReq.Password = utils.RandString(15)
+	err = validate.Struct(newUserReq)
+	validationErrs = err.(validator.ValidationErrors)
+	assert.Len(suite.T(), validationErrs, 1)
+	assert.Equal(suite.T(), "gte", validationErrs[0].Tag())
+
+	// Long Password
+	newUserReq = correctNewUserReq
+	newUserReq.Password = utils.RandString(129)
+	err = validate.Struct(newUserReq)
+	validationErrs = err.(validator.ValidationErrors)
+	assert.Len(suite.T(), validationErrs, 1)
+	assert.Equal(suite.T(), "lte", validationErrs[0].Tag())
 }
 
 func TestUserRequestTestSuite(t *testing.T) {

8f026d3e251ee6cb7414a3cdffad794f75358108
alephshahor <adrianalvarezleon@gmail.com>
2021-04-01 20:19:23 +0100

    feat(requests): Implement UserCredentials request

--- server/requests/user.go
+++ server/requests/user.go
@@ -2,6 +2,12 @@ package requests
 
 type NewUser struct {
 	UserName string `json:"username" validate:"required,gt=0,lte=20"`
-	Password string `json:"password" validate:"required,gte=16,lte=128"`
 	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,gte=16,lte=128"`
+}
+
+type UserCredentials struct {
+	UserName string `json:"username" validate:"gt=0,lte=20"`
+	Email    string `json:"email" validate:"email"`
+	Password string `json:"password" validate:"required,gte=16,lte=128"`
 }
